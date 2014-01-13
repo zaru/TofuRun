99476d1fead100a8cc4e248347606cbf50abbadc
zaru@sakuraba <zarutofu@gmail.com>
2014-01-13 15:55:01 +0900

    ネコ移動アクションを追加

--- Classes/GameLayer.cpp
+++ Classes/GameLayer.cpp
@@ -8,6 +8,27 @@
 
 #include "GameLayer.h"
 
+GameLayer::GameLayer()
+: mBg1(NULL)
+, mBg2(NULL)
+, mBg3(NULL) {
+    
+}
+
+GameLayer::~GameLayer() {
+    CC_SAFE_RELEASE(mBg1);
+    CC_SAFE_RELEASE(mBg2);
+    CC_SAFE_RELEASE(mBg3);
+}
+
+bool GameLayer::onAssignCCBMemberVariable(CCObject* pTarget, const char* pMemberVariableName, CCNode* pNode) {
+    CCB_MEMBERVARIABLEASSIGNER_GLUE(this, "mBg1", CCSprite*, this->mBg1);
+    CCB_MEMBERVARIABLEASSIGNER_GLUE(this, "mBg2", CCSprite*, this->mBg2);
+    CCB_MEMBERVARIABLEASSIGNER_GLUE(this, "mBg3", CCSprite*, this->mBg3);
+    
+    return false;
+}
+
 SEL_MenuHandler GameLayer::onResolveCCBCCMenuItemSelector(CCObject* pTarget, const char* pSelectorName) {
     CCB_SELECTORRESOLVER_CCMENUITEM_GLUE(this, "tappedLeftFoot", GameLayer::tappedLeftFoot);
     CCB_SELECTORRESOLVER_CCMENUITEM_GLUE(this, "tappedRightFoot", GameLayer::tappedRightFoot);
@@ -17,11 +38,33 @@ SEL_MenuHandler GameLayer::onResolveCCBCCMenuItemSelector(CCObject* pTarget, con
 
 void GameLayer::tappedLeftFoot(CCObject* pSender, CCControlEvent pCCControlEvent) {
     CCLOG("left");
+    tappedFoot();
 }
 void GameLayer::tappedRightFoot(CCObject* pSender, CCControlEvent pCCControlEvent) {
     CCLOG("right");
+    tappedFoot();
 }
 
 SEL_CCControlHandler GameLayer::onResolveCCBCCControlSelector(CCObject* pTarget, const char* pSelectorName) {
     return NULL;
+}
+
+void GameLayer::tappedFoot() {
+    const float moveTime = 1.0 / 3;
+    float moveDistanceForBG3 = (this->mBg3->getContentSize().width - CCDirector::sharedDirector()->getWinSize().width) / GOAL_COUNT;
+    
+    CCMoveBy* move1 = CCMoveBy::create(moveTime, ccp(-moveDistanceForBG3 / 4, 0));
+    this->mBg1->runAction(move1);
+    
+    CCMoveBy* move2 = CCMoveBy::create(moveTime, ccp(-moveDistanceForBG3 / 2, 0));
+    this->mBg2->runAction(move2);
+    
+    CCMoveBy* move3 = CCMoveBy::create(moveTime, ccp(-moveDistanceForBG3, 0));
+    this->mBg3->runAction(move3);
+    
+    float moveDistanceForCat = CCDirector::sharedDirector()->getWinSize().width / 2 / GOAL_COUNT;
+    
+    CCNode* cat = this->getChildByTag(1);
+    CCMoveBy* move = CCMoveBy::create(moveTime, ccp(moveDistanceForCat, 0));
+    cat->runAction(move);
 }
--- Classes/GameLayer.h
+++ Classes/GameLayer.h
@@ -15,14 +15,25 @@
 USING_NS_CC;
 USING_NS_CC_EXT;
 
-class GameLayer : public CCLayer, public CCBSelectorResolver {
+#define GOAL_COUNT 100
+
+class GameLayer : public CCLayer, public CCBSelectorResolver, public CCBMemberVariableAssigner {
+    CCSprite* mBg1;
+    CCSprite* mBg2;
+    CCSprite* mBg3;
+    
     virtual SEL_MenuHandler onResolveCCBCCMenuItemSelector(CCObject* pTarget, const char* pSelectorName);
     virtual SEL_CCControlHandler onResolveCCBCCControlSelector(CCObject* pTarget, const char* pSelectorName);
+    virtual bool onAssignCCBMemberVariable(CCObject* pTarget, const char* pMemberVariableName, CCNode* pNode);
     
     void tappedLeftFoot(CCObject* pSender, CCControlEvent pCCControlEvent);
     void tappedRightFoot(CCObject* pSender, CCControlEvent pCCControlEvent);
+    void tappedFoot();
 
 public:
+    GameLayer();
+    virtual ~GameLayer();
+    
     CCB_STATIC_NEW_AUTORELEASE_OBJECT_WITH_INIT_METHOD(GameLayer, create);
 };
 
